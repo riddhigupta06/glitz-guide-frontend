a093a68ffe42d6f0601cd21d3da456995f916037
Riddhi Gupta <riddhig566@gmail.com>
2023-12-04 00:03:43 -0500

    cleaned up

--- src/Details/index.js
+++ src/Details/index.js
@@ -45,7 +45,7 @@ export default function Details() {
                             />
                         </div>
                         <div className="col-8">
-                            
+                            Info
                         </div>
                     </div>
                 </Box>
@@ -53,63 +53,3 @@ export default function Details() {
         </Box>
     )
 }
-
-/**
- * api_featured_image
-: 
-"//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/047/original/open-uri20180708-4-e7idod?1531087336"
-brand
-: 
-"colourpop"
-category
-: 
-"lipstick"
-created_at
-: 
-"2018-07-08T22:01:20.178Z"
-currency
-: 
-"CAD"
-description
-: 
-"Blotted Lip Sheer matte lipstick that creates the perfect popsicle pout! Formula is lightweight, matte and buildable for light to medium coverage."
-id
-: 
-1047
-image_link
-: 
-"https://cdn.shopify.com/s/files/1/1338/0845/products/brain-freeze_a_800x1200.jpg?v=1502255076"
-name
-: 
-"Blotted Lip"
-price
-: 
-"5.5"
-price_sign
-: 
-"$"
-product_api_url
-: 
-"http://makeup-api.herokuapp.com/api/v1/products/1047.json"
-product_colors
-: 
-(7) [{…}, {…}, {…}, {…}, {…}, {…}, {…}]
-product_link
-: 
-"https://colourpop.com/collections/lippie-stix?filter=blotted-lip"
-product_type
-: 
-"lipstick"
-rating
-: 
-null
-tag_list
-: 
-(2) ['cruelty free', 'Vegan']
-updated_at
-: 
-"2018-07-09T00:53:23.287Z"
-website_link
-: 
-"https://colourpop.com"
- */

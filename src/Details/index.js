1e7bb057031a2698cc217762878bbaa7ab45ae4b
Riddhi Gupta <riddhig566@gmail.com>
2023-12-04 13:10:58 -0500

    completed details page

--- src/Details/index.js
+++ src/Details/index.js
@@ -1,4 +1,4 @@
-import { Box, Heading, Text, Spinner, Image } from "@chakra-ui/react";
+import { Box, Heading, Text, Spinner, Image, Badge, Stack, Button } from "@chakra-ui/react";
 import axios from "axios";
 import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
@@ -12,6 +12,8 @@ export default function Details() {
         console.log(response.data)
         setProduct(response.data)
     }
+
+    const colors = ['green', 'yellow', 'blue', 'purple', 'pink', 'red']
 
     useEffect(() => {
         fetchProduct()
@@ -45,7 +47,53 @@ export default function Details() {
                             />
                         </div>
                         <div className="col-8">
-                            Info
+                            <Text fontSize='lg' margin={0}>
+                                <strong>Description</strong>
+                            </Text>
+                            <Text fontSize='lg'>
+                                {product.description}
+                            </Text>
+                            <div style={{display: 'flex', flexDirection: 'row', width:'100%', gap:'50px', marginTop:'40px' }}>
+                                <Stack>
+                                    <Text fontSize='lg' margin={0}>
+                                        <strong>Product Type</strong>
+                                    </Text>
+                                    <Text fontSize='lg'>
+                                        {product.product_type}
+                                    </Text>
+                                </Stack>
+                                <Stack>
+                                    <Text fontSize='lg' margin={0}>
+                                        <strong>Product Category</strong>
+                                    </Text>
+                                    <Text fontSize='lg'>
+                                        {product.category}
+                                    </Text>
+                                </Stack>
+                                {product.tag_list.length > 0 && (
+                                    <Stack>
+                                        <Text fontSize='lg' margin={0}>
+                                            <strong>Tags</strong>
+                                        </Text>
+                                        <div>
+                                            {product.tag_list.map((tag, idx) => <Badge ml='1' key={idx} fontSize='0.8em' colorScheme={colors[idx%colors.length]}>{tag}</Badge>)}
+                                        </div>
+                                    </Stack>
+                                )}
+                            </div>
+                            <Text fontSize='lg' margin={0} marginTop={'20px'}>
+                                <strong>Price</strong>
+                            </Text>
+                            <Text fontSize='lg' margin={0}>
+                                ${(Math.round(product.price * 100) / 100).toFixed(2)}
+                            </Text>
+                            {product.product_link && (
+                                <a href={product.product_link} target="_blank">
+                                    <Button variant='solid' colorScheme='pink' marginTop={'20px'}>
+                                        View product page
+                                    </Button>
+                                </a>
+                            )}
                         </div>
                     </div>
                 </Box>

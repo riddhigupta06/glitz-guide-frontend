4d669093d71fdc7398c37064e5b9574589396ff3
Riddhi Gupta <riddhig566@gmail.com>
2023-12-07 18:27:55 -0500

    removed console logs

--- src/Router/ProtectedRoute.js
+++ src/Router/ProtectedRoute.js
@@ -3,6 +3,5 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ element }) {
     const user = JSON.parse(sessionStorage.getItem('user'));
-    console.log('protected', user)
     return user !== undefined && user !== null && user !== 'null' ? element : <Navigate to={'/login'} replace />;
 }

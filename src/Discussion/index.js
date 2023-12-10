ec93e44e7e86a6e0a37ab31434e34033cb2f9a5f
Neha <ravuri.n@northeastern.edu>
2023-12-10 12:07:57 -0500

    initial

---
+++ src/Discussion/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Box, Button, Spinner, Heading, Badge, IconButton, Tooltip } from "@chakra-ui/react";
+
+export default function Discussion() {
+    return (
+        <Box padding={3}>
+            <Heading as={'h6'} size={'lg'}>Influencer Discussion Board</Heading>
+        </Box>
+    )
+}

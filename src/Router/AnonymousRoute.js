fe5aa067a8fc17cebd2698d148ec03c8435900eb
Riddhi Gupta <riddhig566@gmail.com>
2023-12-12 18:56:56 -0500

    removed imports

--- src/Router/AnonymousRoute.js
+++ src/Router/AnonymousRoute.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export default function AnonymousRoute({ element }) {
     const user = sessionStorage.getItem('user');
-    const navigate = useNavigate()
 
     return user === undefined || user === null || user === 'null' ? element : <Navigate to={'/profile'} replace />;
 }
